401ae6c4e31a63568ba5f3326a0caaa36f6e9d0e
cstavrou <cstavrou@users.noreply.github.com>
2017-09-03 09:27:06 +0100

    Add files via upload

--- Chapter 2/drawing_priors.py
+++ Chapter 2/drawing_priors.py
@@ -1,6 +1,6 @@
+# ====================================================================================
 # Drawing from different priors
-#
-
+# ====================================================================================
 
 from pylab import *
 import matplotlib.pyplot as plt
@@ -8,22 +8,25 @@ import numpy as np
 import tensorflow as tf
 import sys
 from mpl_toolkits.mplot3d.axes3d import Axes3D
+import random
 
 
 # Inputs
-#
-# 1.  tanh/relu/softmax           (activation function)
-# 2.  T/laplace/normal/cauchy/mix (prior function)
-# 3.  n_hidden                    (number of hidden units)
-# 4.  df                          (parameter of T-dist)
-# 5.  samp                        (number of samples of x1, x2)
-# 6.  lim                         (x and y limits for graph)
-# 7.  n_layers                    (number of hidden layers)
-# 8.  std                         (variance of the prior distributions)
-# 9.  seed                        (random seed)
-# 10. n_iter                      (number of iterations for resampling prior)
-# 11. prior1                      (first prior distribution)  [ONLY if prior = mix]  
-# 12. prior2                      (second prior distribution) [ONLY if prior = mix]
+# ====================================================================================
+# 1.  tanh/relu/softmax                        (activation function)
+# 2.  T/laplace/normal/cauchy/mix/spike_slab   (prior function)
+# 3.  n_hidden                   			   (number of hidden units)
+# 4.  df                          			   (parameter of T-dist)
+# 5.  samp                  			       (number of samples of x1, x2)
+# 6.  lim                         			   (x and y limits for graph)
+# 7.  n_layers            				       (number of hidden layers)
+# 8.  std                        			   (variance of the prior distributions)
+# 9.  seed                        			   (random seed)
+# 10. n_iter                 				   (number of iterations for resampling prior)
+# 11. T/F                                      (whether the first layer will have std for priors)
+# 12. prob                   				   (probability for spike_slab prior) [ONLY if prior = spike_slab]
+# 13. prior1                  				   (first prior distribution)  [ONLY if prior = mix]  
+# 14. prior2                   				   (second prior distribution) [ONLY if prior = mix]
 
 
 prior = str(sys.argv[2])
@@ -36,9 +39,8 @@ n_layers = int(sys.argv[7])
 std = float(sys.argv[8])  
 seed = int(sys.argv[9])
 n_iter = int(sys.argv[10])
-
-# Set the random seed for comparisons
-#
+if prior == 'spike_slab':
+	prob = float(sys.argv[12])
 
 # Define the neural network
 #
@@ -74,7 +76,6 @@ if n_layers == 0:
 
 # Create the meshgrid
 #
-
 x1 = np.linspace(-lim, lim, samp)
 x2 = np.linspace(-lim, lim, samp)
 X, Y = np.meshgrid(x1, x2)
@@ -82,7 +83,6 @@ X, Y = np.meshgrid(x1, x2)
 x1 = np.reshape([np.linspace(-lim, lim, samp)]*samp, [-1], order='F')
 x2 = np.reshape([np.linspace(-lim, lim, samp)]*samp, [-1])
 x_in = np.transpose([x1,x2])
-
 
 
 def initialise_par(size, fun, df):
@@ -101,7 +101,40 @@ def initialise_par(size, fun, df):
 	if fun == 'normal':
 		weights = tf.cast(np.random.normal(size=size, scale = std/(size[0])**.5), tf.float32)
 		bias = tf.cast(np.random.normal(size=size_b, scale = std/(size[0])**.5), tf.float32)
+	if fun == 'spike_slab':
+		ber_1 = tf.cast(np.random.binomial(1, prob, size=size), tf.float32)
+		ber_2 = tf.cast(np.random.binomial(1, prob, size=size_b), tf.float32)
+		weights = tf.multiply(ber_1, tf.cast(np.random.normal(size=size, 
+			scale = std/(size[0])**.5), tf.float32))
+		bias = tf.multiply(ber_2, tf.cast(np.random.normal(size=size_b, 
+			scale = std/(size[0])**.5), tf.float32))
 	return tf.Variable(weights), tf.Variable(bias)
+
+def initialise_par_1(size, fun, df):
+	size_b = [size[len(size) - 1]]
+	if fun == 'T':
+		weights = tf.cast(np.random.standard_t(df, size=size), tf.float32)
+		bias = tf.cast(np.random.standard_t(df, size=size_b), tf.float32)
+	if fun == 'cauchy':
+		weights = tf.cast(np.random.standard_cauchy(size=size), tf.float32)
+		bias = tf.cast(np.random.standard_cauchy(size=size_b), tf.float32)
+	if fun == 'laplace':
+		weights = tf.cast(np.random.laplace(size=size, scale = 1), tf.float32)
+		bias = tf.cast(np.random.laplace(size=size_b, scale = 1), tf.float32)
+		# weights = tf.cast(np.random.laplace(size=size, scale = std/size[0]**.5), tf.float32)
+		# bias = tf.cast(np.random.laplace(size=size_b, scale = std/size[0]**.5), tf.float32)
+	if fun == 'normal':
+		weights = tf.cast(np.random.normal(size=size, scale = 1), tf.float32)
+		bias = tf.cast(np.random.normal(size=size_b, scale = 1), tf.float32)
+	if fun == 'spike_slab':
+		ber_1 = tf.cast(np.random.binomial(1, prob, size=size), tf.float32)
+		ber_2 = tf.cast(np.random.binomial(1, prob, size=size_b), tf.float32)
+		weights = tf.multiply(ber_1, tf.cast(np.random.normal(size=size, 
+			scale = 1), tf.float32))
+		bias = tf.multiply(ber_2, tf.cast(np.random.normal(size=size_b, 
+			scale = 1), tf.float32))
+	return tf.Variable(weights), tf.Variable(bias)
+
 
 if n_layers == 0:
 	W_0, b_0 = initialise_par([D, 1], prior, df)
@@ -109,16 +142,21 @@ if n_layers == 1:
 	W_0, b_0 = initialise_par([D, n_hidden], prior, df)
 	W_1, b_1 = initialise_par([n_hidden, 1], prior, df)	
 if n_layers == 2 and prior != 'mix':
-	W_0, b_0 = initialise_par([D, n_hidden], prior, df)
+	if str(sys.argv[11]) == 'T':
+		W_0, b_0 = initialise_par_1([D, n_hidden], prior, df)
+	else:
+		W_0, b_0 = initialise_par([D, n_hidden], prior, df)
 	W_1, b_1 = initialise_par([n_hidden, n_hidden], prior, df)
 	W_2, b_2 = initialise_par([n_hidden, 1], prior, df)
 if prior == 'mix':
-	prior1 = str(sys.argv[11])
-	prior2 = str(sys.argv[12])	
-	W_0, b_0 = initialise_par([D, n_hidden], prior1, df)
+	prior1 = str(sys.argv[13])
+	prior2 = str(sys.argv[14])
+	if str(sys.argv[11]) == 'T':	
+		W_0, b_0 = initialise_par_1([D, n_hidden], prior1, df)
+	else:
+		W_0, b_0 = initialise_par([D, n_hidden], prior1, df)
 	W_1, b_1 = initialise_par([n_hidden, n_hidden], prior1, df)
 	W_2, b_2 = initialise_par([n_hidden, 1], prior2, df)
-
 
 
 x = tf.placeholder(tf.float32, [None, None])
@@ -141,7 +179,8 @@ with tf.device('/cpu:0'):
 			with tf.Session() as sess:
 				sess.run(tf.global_variables_initializer())
 				y_i = sess.run(y_, feed_dict={x: x_in})	
-		print(i)
+		if (i+1)%100 == 0:
+			print(i+1)
 		if i == 0:
 			y_new = y_i
 		if i > 0:
@@ -149,16 +188,17 @@ with tf.device('/cpu:0'):
 
 y = y_new/n_iter
 
-
 Z = np.reshape(y, [samp, samp])
 
 fig = plt.figure(figsize=(8,6))
-
 ax = fig.add_subplot(1,1,1, projection='3d')
-
+ax.xaxis._axinfo["grid"]['linewidth'] = 0.1
+ax.yaxis._axinfo["grid"]['linewidth'] = 0.1
+ax.zaxis._axinfo["grid"]['linewidth'] = 0.1
 # ax.plot_surface(X, Y, Z, rstride=4, cstride=4, alpha=0.25)
 #ax.plot_wireframe(X, Y, Z, rstride=4, cstride=4)
-ax.plot_surface(X, Y, Z, rstride=4, cstride=4, alpha=1, cmap=cm.CMRmap)
+ax.plot_surface(X, Y, Z, rstride=4, cstride=4, alpha=1, cmap=cm.CMRmap, 
+	edgecolors='black')
 # cset = ax.contour(X, Y, Z, zdir='z', offset=-1, cmap=cm.coolwarm)
 # cset = ax.contour(X, Y, Z, zdir='x', offset=-1, cmap=cm.coolwarm)
 # cset = ax.contour(X, Y, Z, zdir='y', offset=4, cmap=cm.coolwarm)
